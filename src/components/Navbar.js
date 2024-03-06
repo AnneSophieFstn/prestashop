39e9f7ad10a7ed9261203f8e625f1aaa9467f8d3
AnneSophieFstn <annesophie.faustino@gmail.com>
2024-03-06 14:57:24 +0400

    update view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,19 +94,6 @@ export default function Navbar() {
                               "block px-4 py-2 text-sm text-gray-700"
                             )}
                           >
-                            Paramètres
-                          </Link>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            to="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
                             Deconnexion
                           </Link>
                         )}
