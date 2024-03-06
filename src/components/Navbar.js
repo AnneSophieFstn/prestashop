0d5a314eb0b33004ff038f90477c24bd3af5a1ed
AnneSophieFstn <annesophie.faustino@gmail.com>
2024-03-06 10:46:35 +0400

    Navbar + logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,6 @@ import { Link } from "react-router-dom";
 
 const navigation = [
   { name: "Accueil", to: "/home", current: true },
-  { name: "Annuaire", to: "/annuaire", current: false },
-  { name: "Secteur d'activités", to: "/secteur-activite", current: false },
 ];
 
 function classNames(...classes) {
@@ -34,41 +32,22 @@ export default function Navbar() {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                  />
-                </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
+                {navigation.map((item) => (
                       <Link
                         key={item.name}
                         to={item.to}
-                        className={classNames(
-                          item.current
-                            ? "bg-gray-900 text-white"
-                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                          "rounded-md px-3 py-2 text-sm font-medium"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
                       >
-                        {item.name}
+                  <img
+                    className="h-8 w-auto"
+                    src="/assets/prestalogo.png"
+                    alt="Your Company"
+                  />
                       </Link>
                     ))}
-                  </div>
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
+
 
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
