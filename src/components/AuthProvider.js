8441eb156f4adcb251970b61990aa24fb385f3f8
Anne-Sophie <annesophie.faustino@gmail.com>
2024-03-12 22:33:06 +0400

    createContext auth

--- src/components/AuthProvider.js
+++ src/components/AuthProvider.js
@@ -1,12 +1,56 @@
-import React, { createContext, useState, useContext } from 'react';
+import axios from "axios";
+import React, { createContext, useState, useContext } from "react";
+import { useNavigate } from "react-router";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const login = async (email, password) => {
+    try {
+      const response = await axios.post("http://127.0.0.1:3001/login", {
+        email,
+        password,
+      });
+
+      const token = response.data.token;
+
+      console.log("token: ", token);
+      if (token) {
+        localStorage.setItem("token", response.data.token);
+        await me();
+      }
+    } catch (error) {
+      console.log(error.response.data.message);
+    }
+  };
+
+  const me = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axios.get("http://127.0.0.1:3001/user", {
+        headers: {
+          Authorization: `Bearer ${token}`, // Envoyer le token JWT dans l'en-tête de la requête
+        },
+      });
+      console.log("response.data: ", response.data);
+      setUser(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+  };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+    <AuthContext.Provider
+      value={{ isAuthenticated, setIsAuthenticated, login, me, logout, user }}
+    >
       {children}
     </AuthContext.Provider>
   );
